af1507f1b8000451f652ea7ff20b33fdace8ab37
jajac13 <milosjajac13@gmail.com>
2017-02-11 18:51:51 +0100

    server static

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 var app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.post('/api/please', (req, res) => {
     res.status(200).send({ msg: 'Thank you!' });
 });
