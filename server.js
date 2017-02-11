c57fb52aa190a72ef52d88c35effd8f1d8687fe5
jajac13 <milosjajac13@gmail.com>
2017-02-11 18:53:45 +0100

    change port

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.post('/api/please', (req, res) => {
     res.status(200).send({ msg: 'Thank you!' });
 });
 
-app.listen(3001, () => {
-    console.log('Server listening on port 3001.');
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Server listening.');
 });
